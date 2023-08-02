a3c066eeae56c7fe7ce7bb0b2ed158b57ca442df
mcgov <matthew@mcgov.dev>
2023-08-02 11:19:48 -0700

    update Jammy mana SIG image installation script

--- mana_test_setup.sh
+++ mana_test_setup.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# install dependencies and build/install:
+# MANA compatible kernel, rdma-core, and dpdk
+# for ubuntu server 22.04
+
 function assert_success {
     if [ $? -ne 0 ]; then
         echo "Last call failed! Exiting..."
@@ -17,26 +21,26 @@ function check_and_append {
 }
 
 export DEBIAN_FRONTEND=noninteractive
-# install dependencies for ubuntuserver 22.04
 sudo apt update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -q -y build-essential cmake gcc libudev-dev \
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -q -y build-essential cmake libudev-dev \
 libnl-3-dev libnl-route-3-dev ninja-build pkg-config \
 valgrind python3-dev cython3 python3-docutils pandoc \
-build-essential flex bison libssl-dev unzip \
+flex bison libssl-dev \
 libelf-dev python3-pip meson dwarves libnuma-dev libpcap-dev
-
-
 assert_success
 
 pip3 install pyelftools
-if [ -z "$GIT_SOURCE" ]; then
-    GIT_SOURCE="https://github.com/longlimsft/linux.git"
+
+# pick Linux repo and tag to build, 6.4 release has all the vpci and mana bits.
+if [ -z "$LINUIX_GIT_SOURCE" ]; then
+    LINUIX_GIT_SOURCE="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 fi
-if [ -z "$GIT_REFERENCE" ]; then
-    GIT_REFERENCE="rdma-fix"
+if [ -z "$LINUX_GIT_REFERENCE" ]; then
+    LINUX_GIT_REFERENCE="v6.4"
 fi
-# Build/install 6.2-rc2
-git clone $GIT_SOURCE -b $GIT_REFERENCE --depth 1
+
+# Build/install Linux kernel
+git clone $LINUIX_GIT_SOURCE -b $LINUX_GIT_REFERENCE --depth 1
 assert_success
 pushd linux
 #git checkout v6.2-rc2
@@ -47,7 +51,7 @@ sed -i 's/CONFIG_SYSTEM_TRUSTED_KEYS/#CONFIG_SYSTEM_TRUSTED_KEYS/g' .config
 sed -i 's/# CONFIG_MANA_INFINIBAND is not set/CONFIG_MANA_INFINIBAND=m/g' .config
 #check that it's set
 check_and_append .config 'CONFIG_MANA_INFINIBAND=m'
-#build it
+# build it
 yes "" | make -j 12
 assert_success
 sudo make modules_install
@@ -57,25 +61,26 @@ assert_success
 popd
 
 # build/install rdma-core 44
-wget https://github.com/linux-rdma/rdma-core/releases/download/v44.0/rdma-core-44.0.tar.gz
+wget https://github.com/linux-rdma/rdma-core/releases/download/v46.0/rdma-core-46.0.tar.gz
 assert_success
-tar xzvf rdma-core-44.0.tar.gz
+tar xzvf rdma-core-46.0.tar.gz
 assert_success
-pushd rdma-core-44.0/
+pushd rdma-core-46.0/
 assert_success
+# cursed not-for-production rdma-core installation from source YMMV
 cmake -DIN_PLACE=0 -DNO_MAN_PAGES=1 -DCMAKE_INSTALL_PREFIX=/usr
 assert_success
-sudo make -j 12
+sudo make -j 28
 assert_success
 sudo make install
 assert_success
 popd 
 
 if [ -z "$DPDK_GIT_SOURCE" ]; then
-    DPDK_GIT_SOURCE="https://github.com/longlimsft/dpdk.git"
+    DPDK_GIT_SOURCE="https://github.com/DPDK/dpdk.git"
 fi;
 if [ -z "$DPDK_GIT_REF" ]; then
-    DPDK_GIT_REF="longli/submit_patch_cpu_cycles"
+    DPDK_GIT_REF="v23.07-rc3"
 fi
 # build / install dpdk 22.11  (already will be in lisa working dir)
 git clone $DPDK_GIT_SOURCE
@@ -83,11 +88,6 @@ assert_success
 pushd dpdk
 git checkout $DPDK_GIT_REF
 assert_success
-
-# # uncomment for dpdk branches that don't have mana enabled. 
-# # Long's already have it, upstream might not.
-# sed -i "s/'af_packet',/'af_packet',\n        'mana',/g" drivers/net/meson.build
-
 meson build -Dexamples=l3fwd
 assert_success
 
@@ -103,13 +103,8 @@ echo 'mana_ib' | sudo tee -a /etc/modules
 
 echo "SETUP AND BUILD COMPLETE"
 
-#yes 'y' | sudo waagent deprovision+user
-
-# rg=mamcgove-dev-test
-# gallery=mamcgove_mana_dpdk
-# image=mana-dpdk-22-04
-# disks=`az disk list --resource-group $rg` ## doesn't work
-# version=`az sig image-version list --gallery-name $gallery --gallery-image-definition $image --resource-group $rg` | jq.[-1].name
-# split=
-# $new_version = @($split.P1, $split.P2, ($split.P3 + 1)) -join "."
-# az sig image-version create --resource-group $rg --gallery-name $gallery --gallery-image-definition $image --gallery-image-version $new_version --os-snapshot $disks[0].id
+## run manually to genericize if creating an azure sig image
+# sudo -s
+# waagent deprovision+user 
+#### asks for 'y' ^
+# shutdown now
