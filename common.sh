cb0ad2f685a3c8a83c2dd8f39412ef1d94eee85c
mcgov <matthew@mcgov.dev>
2023-09-07 10:27:33 -0700

    Create common.sh

---
+++ common.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+function assert_success {
+    if [ $? -ne 0 ]; then
+        echo "Last call failed! Exiting..."
+        exit -1
+    fi
+}
+
+function check_and_append {
+    #check that it's set
+    cat $1 | grep "$2"
+    #append if it wasn't present
+    if [ $? -ne 0 ]; then
+        echo "$2" >> $1
+    fi;
+}
