47e55756a9f4713bad2ebc229a956fbdafbd7b30
mcgov <matthew@mcgov.dev>
2024-03-01 13:02:04 -0800

    Test supporting re-runs

--- dpdk-helpers/pps-dpdk-testpmd.sh
+++ dpdk-helpers/pps-dpdk-testpmd.sh
@@ -13,42 +13,6 @@ fi
 SEND_IP="$2"   #ip addr of sender (needed on sender only)
 RECV_IP="$3"   # ip addr of receiver (needed on sender only)
 # Assuming use of eth1 for DPDK in this demo
-PRIMARY="eth1"
-# $ ip -br link show master eth1 
-# > enP30832p0s0     UP             f0:0d:3a:ec:b4:0a <... # truncated
-# grab interface name for device bound to primary
-echo "Checking for lower interface for eth1..."
-SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
-echo "Note: Test expects 2 NICs so if this call fails either:"
-echo " - the NIC was already bound to netvsc, restart the node and run this script again."
-echo " or:"
-echo " - The VM was deployed with 1 NIC. Add another AccelNet enabled NIC."
-assert_success
-
-# Get mac address for MANA interface (should match primary)
-export MANA_MAC="`ip -br link show master $PRIMARY | awk '{ print $3 }'`"
-# $ ethtool -i enP30832p0s0 | grep bus-info
-# > bus-info: 7870:00:00.0
-# get MANA device bus info to pass to DPDK
-export BUS_INFO="`ethtool -i $SECONDARY | grep bus-info | awk '{ print $2 }'`"
-
-# Set MANA interfaces DOWN before starting DPDK
-sudo ip link set $PRIMARY down
-sudo ip link set $SECONDARY down
-## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
-DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
-assert_success
-NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
-echo "loading uio_hv_generic driver..."
-sudo modprobe uio_hv_generic
-assert_success
-echo $NET_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/new_id
-assert_success
-echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/hv_netvsc/unbind
-assert_success
-echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/bind
-assert_success
-VDEV_ARG="--vdev=$BUS_INFO,mac=$MANA_MAC"
 
 if [[ "$FWDMODE" == "rxonly" ]] || [[ -z "$SEND_IP" ]] || [[ -z "$RECV_IP" ]]; then
     SENDER_IP_ARG=""
@@ -70,8 +34,34 @@ else
 fi
 let LAST_CORE=$FWD_CORES+1
 
+PRIMARY="eth1"
+echo "running netvsc pmd setup..."
+if [[ -f "/sys/class/net/$PRIMARY" ]]; then
+    ./setup-netvsc-pmd.sh eth1
+    assert_success
+else
+    echo "eth1 not found, assuming re-run and attempt to set lower down..."
+    # otherwise, this is a re-run, attempt to set the lower interface down.
+    LOWER="`cat ./$PRIMARY.lower.nic`"
+    if [[ -z "$LOWER" ]]; then
+        echo "Note file: ./$PRIMARY.lower.nic not found. Setup is broken, reach out to the maintainer."
+        exit -1
+    fi
+    ip link set $LOWER down
+fi
+
+# Setup should place an argument file with the stuff needed to run testpmd
+# Or, it's already there.
+if [[ -f "./$PRIMARY-dpdk-eal-vdev.arg" ]]; then
+    VDEV_ARG="`cat ./$PRIMARY-dpdk-eal-vdev.arg`"
+else
+    echo "There was a problem fetching the DPDK EAL vdev argument for $PRIMARY"
+    echo "Reach out to the maintainer and share your logs."
+    exit -1
+fi
+
 RUN_DPDK_CMD="sudo timeout -s INT 120 dpdk-testpmd -l 1-$LAST_CORE $VDEV_ARG -- --forward-mode=$FWDMODE --auto-start --nb-cores=$FWD_CORES  --txd=128 --rxd=128 --txq=32 --rxq=32 --stats 2  $SENDER_IP_ARG"
-echo $RUN_DPDK_CMD >> ./rerun-dpdk-testpmd
+echo $RUN_DPDK_CMD | tee -a ./rerun-dpdk-testpmd
 
 # MANA multiple queue test (example assumes > 64 cores)
 $RUN_DPDK_CMD
---
+++ dpdk-helpers/setup-netvsc-pmd.sh
@@ -0,0 +1,58 @@
+PRIMARY=$1
+
+# $ ip -br link show master eth1 
+# > enP30832p0s0     UP             f0:0d:3a:ec:b4:0a <... # truncated
+# grab interface name for device bound to primary
+echo "Checking for lower interface for eth1..."
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+echo "Note: Test expects 2 NICs so if this call fails either:"
+echo " - the NIC was already bound to netvsc, restart the node and run this script again."
+echo " or:"
+echo " - The VM was deployed with 1 NIC. Add another AccelNet enabled NIC."
+assert_success
+
+# Get mac address for MANA interface (should match primary)
+export MANA_MAC="`ip -br link show master $PRIMARY | awk '{ print $3 }'`"
+# $ ethtool -i enP30832p0s0 | grep bus-info
+# > bus-info: 7870:00:00.0
+# get MANA device bus info to pass to DPDK
+export BUS_INFO="`ethtool -i $SECONDARY | grep bus-info | awk '{ print $2 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+sudo ip link set $PRIMARY down
+sudo ip link set $SECONDARY down
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+assert_success
+NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
+echo "loading uio_hv_generic driver..."
+sudo modprobe uio_hv_generic
+assert_success
+echo $NET_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+assert_success
+echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/hv_netvsc/unbind
+assert_success
+echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/bind
+assert_success
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+assert_success
+NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
+echo "loading uio_hv_generic driver..."
+sudo modprobe uio_hv_generic
+assert_success
+echo $NET_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+assert_success
+echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/hv_netvsc/unbind
+assert_success
+echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/bind
+assert_success
+VDEV_ARG="--vdev=$BUS_INFO,mac=$MANA_MAC"
+VDEV_ARG_FILE="$PRIMARY-dpdk-eal-vdev.arg"
+VDEV_NOTE_FILE="$PRIMARY.lower.nic"
+echo "Writing argument to $VDEV_ARG_FILE..."
+echo $VDEV_ARG | tee ./$VDEV_ARG_FILE
+echo "Writing association file to $VF_NOTE_FILE"
+echo $SECONDARY | tee ./$VF_NOTE_FILE
+echo "Setup complete! w00t"
