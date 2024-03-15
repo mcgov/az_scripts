5f00ff776449567b22e862c8c8fa8ffb90efa002
Matthew McGovern <matthew@mcgov.dev>
2024-03-15 22:48:15 +0000

    shellcheck: l3fwd

--- dpdk-helpers/l3fwd/create_l3fwd_rules_files.sh
+++ dpdk-helpers/l3fwd/create_l3fwd_rules_files.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-SEND_IP_CIDR=$1
+SEND_IP_CIDR="$1"
 #SEND_PORT=$2
-RCV_IP_CIDR=$2
+RCV_IP_CIDR="$2"
 #RCV_PORT=$4
 
 
-pushd ../dpdk/build/examples
-echo "R $1 2" | tee rules_v4
-echo "R $2 3" | tee -a rules_v4
+pushd ../dpdk/build/examples || ( echo "pushd ../dpdk/build/examples failed: $?"; exit 1)
+echo "R $SEND_IP_CIDR 2" | tee rules_v4
+echo "R $RCV_IP_CIDR 3" | tee -a rules_v4
 echo "" | tee rules_v6 #try an empty file
 #echo "R $6 $4" | tee -a rules_v6
--- dpdk-helpers/l3fwd/run-dpdk-l3fwd.sh
+++ dpdk-helpers/l3fwd/run-dpdk-l3fwd.sh
@@ -1,21 +1,12 @@
 #! /bin/bash
 
-DPDK_APP_PATH=$1
+DPDK_APP_PATH="$1"
 
 function assert_success {
     if [ $? -ne 0 ]; then
         echo "Last call failed! Exiting..."
-        exit -1
+        exit 1
     fi
-}
-
-function check_and_append {
-    #check that it's set
-    cat $1 | grep "$2"
-    #append if it wasn't present
-    if [ $? -ne 0 ]; then
-        echo "$2" >> $1
-    fi;
 }
 
 echo "preparing for netvsc pmd use"
@@ -27,40 +18,43 @@ MAC_INFO=""
 modprobe uio_hv_generic
 assert_success
 NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
-echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo "$NET_UUID" > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
 
 for nic in eth1 eth2;
 do
     # $ ip -br link show master eth1 
     # > enP30832p0s0     UP             f0:0d:3a:ec:b4:0a <... # truncated
     # grab interface name for device bound to primary
-    SECONDARY="`ip -br link show master $nic | awk '{ print $1 }'`"
+    SECONDARY=$(ip -br link show master "$nic" | awk '{ print $1 }')
     # Get mac address for MANA interface (should match primary)
-    export MANA_MAC="`ip -br link show master $nic | awk '{ print $3 }'`"
+    MANA_MAC=$(ip -br link show master "$nic" | awk '{ print $3 }')
     # $ ethtool -i enP30832p0s0 | grep bus-info
     # > bus-info: 7870:00:00.0
     # get MANA device bus info to pass to DPDK
-    export BUS_INFO="`ethtool -i $SECONDARY | grep bus-info | awk '{ print $2 }'`"
+    DEVICE_INFO=$(ethtool -i "$SECONDARY")
+    BUS_INFO_RAW=$(echo "$DEVICE_INFO" | grep bus-info)
+    BUS_INFO=$(echo "$BUS_INFO_RAW" | awk '{ print $2 }')
 
 
     ## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
-    DEV_UUID=$(basename $(readlink /sys/class/net/$nic/device))
-    echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
-    echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+    NIC_DEV=$(readlink /sys/class/net/"$nic"/device)
+    DEV_UUID=$(basename "$NIC_DEV")
+    echo "$DEV_UUID" | sudo tee /sys/bus/vmbus/drivers/hv_netvsc/unbind
+    echo "$DEV_UUID" | sudo tee /sys/bus/vmbus/drivers/uio_hv_generic/bind
     if [[ -z "$MAC_INFO" ]]; then
         MAC_INFO="mac=$MANA_MAC"
     else
         MAC_INFO="$MAC_INFO,mac=$MANA_MAC"
     fi
     # Set MANA interfaces DOWN before starting DPDK
-    sudo ip link set $SECONDARY down
+    sudo ip link set "$SECONDARY" down
 done
 
 VDEV_ARG="--vdev=$BUS_INFO,$MAC_INFO"
 
 echo "Use $VDEV_ARG for l3fwd EAL argument"
 # TODO: wrong command
-let LAST_CORE=1
+((LAST_CORE=1))
 QUEUE_CONFIG=""
 for q in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do 
     if [[ -z "$QUEUE_CONFIG" ]]; then
@@ -68,10 +62,10 @@ for q in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
     else
         QUEUE_CONFIG="$QUEUE_CONFIG,($q,$LAST_CORE,2),($q,$LAST_CORE,3)";
     fi
-    let LAST_CORE=$LAST_CORE+1;
+    ((LAST_CORE=LAST_CORE+1))
 done
 
 echo "Running multiple queue test, needs >= 8 cores"
-DPDK_COMMAND="sudo timeout 1200 $DPDK_APP_PATH -l 1-17 $VDEV_ARG -- -p 0xC  --lookup=lpm --config=\"$QUEUE_CONFIG\" --rule_ipv4=rules_v4  --rule_ipv6=rules_v6 --mode=poll --parse-ptype"
-echo $DPDK_COMMAND | tee ./rerun_l3fwd
-$DPDK_COMMAND
+DPDK_COMMAND="sudo timeout 1200 $DPDK_APP_PATH -l 1-17 $VDEV_ARG -- -p 0xC  --lookup=lpm --config=$QUEUE_CONFIG --rule_ipv4=rules_v4  --rule_ipv6=rules_v6 --mode=poll --parse-ptype"
+echo "$DPDK_COMMAND" | tee ./rerun_l3fwd
+sudo timeout 1200 "$DPDK_APP_PATH" -l 1-17 "$VDEV_ARG" -- -p 0xC  --lookup=lpm --config="$QUEUE_CONFIG" --rule_ipv4=rules_v4  --rule_ipv6=rules_v6 --mode=poll --parse-ptype
