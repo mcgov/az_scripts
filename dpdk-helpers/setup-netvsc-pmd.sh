f91030c2b8da819c7bbac4c62f4cc19f0255744d
Matthew McGovern <matthew@mcgov.dev>
2024-03-15 18:38:00 +0000

    shellcheck: setup-netvsc-pmd.sh

--- dpdk-helpers/setup-netvsc-pmd.sh
+++ dpdk-helpers/setup-netvsc-pmd.sh
@@ -4,7 +4,7 @@ function assert_success {
     if [ $? -ne 0 ]; then
         echo "Last call failed! Exiting..."
         ./display-maintainer-info.sh
-        exit -1
+        exit 1
     fi
 }
 
@@ -22,9 +22,9 @@ else
     echo "If this is a re-run after a reboot, use pps-dpdk-testpmd.sh"
     echo "If you already did do that, this script is broken."
     ./display-maintainer-info.sh
-    exit -1
+    exit 1
 fi
-SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+SECONDARY=$(ip -br link show master "$PRIMARY" | awk '{ print $1 }')
 echo "Note: Test expects 2 NICs so if this call fails either:"
 echo " - the NIC was already bound to netvsc, restart the node and run this script again."
 echo " or:"
@@ -32,33 +32,42 @@ echo " - The VM was deployed with 1 NIC. Add another AccelNet enabled NIC."
 assert_success
 
 # Get mac address for MANA interface (should match primary)
-export MANA_MAC="`ip -br link show master $PRIMARY | awk '{ print $3 }'`"
+GET_SECONDARY=$(ip -br link show master "$PRIMARY")
+assert_success
+MANA_MAC=$( echo "$GET_SECONDARY" | awk '{ print $3 }')
+assert_success
 # $ ethtool -i enP30832p0s0 | grep bus-info
 # > bus-info: 7870:00:00.0
 # get MANA device bus info to pass to DPDK
-export BUS_INFO="`ethtool -i $SECONDARY | grep bus-info | awk '{ print $2 }'`"
-
+SECONDARY_INFO=$(ethtool -i "$SECONDARY")
+assert_success
+BUS_INFO_RAW=$(echo "$SECONDARY_INFO" | grep bus-info)
+assert_success
+BUS_INFO=$(echo "$BUS_INFO_RAW" | awk '{ print $2 }')
+assert_success
 # Set MANA interfaces DOWN before starting DPDK
-sudo ip link set $PRIMARY down
-sudo ip link set $SECONDARY down
+sudo ip link set "$PRIMARY" down
+sudo ip link set "$SECONDARY" down
 ## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
-DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+PRIMARY_DEVICE=$(readlink "/sys/class/net/$PRIMARY/device")
+assert_success
+DEV_UUID=$(basename "$PRIMARY_DEVICE")
 assert_success
 NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
 echo "loading uio_hv_generic driver..."
 sudo modprobe uio_hv_generic
 assert_success
-echo $NET_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo "$NET_UUID" | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/new_id
 assert_success
-echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo "$DEV_UUID" | sudo tee -a /sys/bus/vmbus/drivers/hv_netvsc/unbind
 assert_success
-echo $DEV_UUID | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/bind
+echo "$DEV_UUID" | sudo tee -a /sys/bus/vmbus/drivers/uio_hv_generic/bind
 assert_success
 VDEV_ARG="--vdev=$BUS_INFO,mac=$MANA_MAC"
 VDEV_ARG_FILE="$PRIMARY.dpdk-eal-vdev.arg"
 VF_NOTE_FILE="$PRIMARY.lower.nic"
 echo "Writing argument to $VDEV_ARG_FILE..."
-echo $VDEV_ARG | tee ./$VDEV_ARG_FILE
+echo "$VDEV_ARG" | tee ./"$VDEV_ARG_FILE"
 echo "Writing association file to $VF_NOTE_FILE"
-echo $SECONDARY | tee ./$VF_NOTE_FILE
+echo "$SECONDARY" | tee ./"$VF_NOTE_FILE"
 echo "Setup complete! w00t"
