31291547f0cd092f6939d0bedd562ff21700b313
Matthew G. McGovern <matthew@mcgov.dev>
2024-03-19 16:31:25 -0700

    MISSING SPACE

--- dpdk-helpers/l3fwd/run-dpdk-l3fwd.sh
+++ dpdk-helpers/l3fwd/run-dpdk-l3fwd.sh
@@ -74,4 +74,4 @@ echo "Running multiple queue test, needs >= 8 cores"
 DPDK_COMMAND="sudo timeout 1200 $DPDK_APP_EXEC -l 1-17 $VDEV_ARG -- -p 0xC  --lookup=lpm --config=$QUEUE_CONFIG --rule_ipv4=$DPDK_RULES_V4  --rule_ipv6=$DPDK_RULES_V6 --mode=poll --parse-ptype"
 echo "$DPDK_COMMAND" | tee ./rerun_l3fwd
 cd "$DPDK_APP_PATH" || ( echo "CD to $DPDK_APP_PATH failed!: $?"; exit 1;)
-sudo nohup timeout 1200 "$DPDK_APP_EXEC" -l "1-17" "$VDEV_ARG" -- -p "0xC"  --lookup=lpm --config="'$QUEUE_CONFIG'"--rule_ipv4="$DPDK_RULES_V4"  --rule_ipv6="$DPDK_RULES_V6" --mode=poll --parse-ptype &
+sudo nohup timeout 1200 "$DPDK_APP_EXEC" -l "1-17" "$VDEV_ARG" -- -p "0xC"  --lookup=lpm --config="'$QUEUE_CONFIG'" --rule_ipv4="$DPDK_RULES_V4"  --rule_ipv6="$DPDK_RULES_V6" --mode=poll --parse-ptype &
