621fbc4a8eccc5c40346234d46d65d517f2c9d4d
Matthew G. McGovern <matthew@mcgov.dev>
2024-03-19 20:54:41 -0700

    don't cd in l3fwd

--- dpdk-helpers/l3fwd/run-dpdk-l3fwd.sh
+++ dpdk-helpers/l3fwd/run-dpdk-l3fwd.sh
@@ -74,10 +74,11 @@ done
 echo "Running multiple queue test, needs >= 8 cores"
 DPDK_COMMAND="sudo timeout 1200 $DPDK_APP_EXEC -l 1-17 $VDEV_ARG -- -p 0xC  --lookup=lpm --config=$QUEUE_CONFIG --rule_ipv4=$DPDK_RULES_V4  --rule_ipv6=$DPDK_RULES_V6 --mode=poll --parse-ptype"
 echo "$DPDK_COMMAND" | tee ./rerun_l3fwd
-cd "$DPDK_APP_PATH" || ( echo "CD to $DPDK_APP_PATH failed!: $?"; exit 1;)
+
 # much fun figuring out these escaping rules
-nohup sudo "$DPDK_APP_EXEC" -l "1-17" "$VDEV_ARG" "--" -p 0xC  --lookup=lpm --config="$QUEUE_CONFIG" --rule_ipv4="$DPDK_RULES_V4"  --rule_ipv6="$DPDK_RULES_V6" --mode=poll --parse-ptype &
+sudo nohup "$DPDK_APP_EXEC" -l "1-17" "$VDEV_ARG" -- -p 0xC --lookup=lpm --config="$QUEUE_CONFIG" --rule_ipv4="$DPDK_RULES_V4" --rule_ipv6="$DPDK_RULES_V6" --mode=poll --parse-ptype 2>&1 &
 echo "Launched nohup? $?"
-cat ./nohup.out
-echo $(pwd)
-ls $(pwd)
+sudo cat ./nohup.out
+sudo cat ~/nohup.out
+echo "$(pwd)"
+ls "$(pwd)"
